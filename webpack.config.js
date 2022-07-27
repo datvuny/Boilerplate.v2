91fa7d9bc5e6704cc3abec5f5e62c43a2a30633d
Dat Vu <datqvu295@gmail.com>
2022-07-27 16:29:41 -0400

    first commmit

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,24 @@
 module.exports = {
-    entry: './client/index.js',
-    output: {
-      path: __dirname,
-      filename: './public/bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /(node_modules|bower_components)/,
-          loader: 'babel-loader',
-          options: {
-            presets: ['react', 'es2015']
-          }
-        },
-        // use the style-loader/css-loader combos for anything matching the .css extension
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader',
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-react'
           ]
         }
-      ]
-    }
-  };
+      }
+    ]
+  }
+}
